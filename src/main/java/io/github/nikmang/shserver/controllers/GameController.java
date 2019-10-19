307f21937558625a6b720997f4c6425a1702b5a0
nikmanG <ngilevskiy@gmail.com>
2019-10-19 16:39:45 -0400

    Add Game controller tests

--- src/main/java/io/github/nikmang/shserver/controllers/GameController.java
+++ src/main/java/io/github/nikmang/shserver/controllers/GameController.java
@@ -6,6 +6,7 @@ import io.github.nikmang.shserver.game.GameState;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
 
 public class GameController {
@@ -24,23 +25,69 @@ public class GameController {
         cardsInPlay = Collections.emptyList();
     }
 
+    /**
+     * Gets the cards that are currently in play.
+     * If no cards currently in play then returns 3 new cards from the deck.
+     *
+     * @return unmodifiable list of current cards in play.
+     */
     public List<GameDeck.Card> getCardsInPlay() {
         if(cardsInPlay.isEmpty()) {
             cardsInPlay = gameDeck.getCards(3);
         }
 
-        //This should be immutable
-        return cardsInPlay;
+        return Collections.unmodifiableList(cardsInPlay);
     }
 
+    /**
+     * Remove the card at specified index from the game, into the discarded pile.
+     *
+     * @param index Index of the card bust be within the list of provided cards (e.g 3 cards means can only choose 0,1,2)
+     * @return <b>true</b> if index corresponded to a card and could be removed.
+     */
     public boolean removeCardFromPlay(int index) {
-        if(index >= cardsInPlay.size())
+        if(index >= cardsInPlay.size() || index < 0)
             return false;
 
         GameDeck.Card c = cardsInPlay.remove(index);
 
         gameDeck.addCardToDiscardPile(c);
 
+        return true;
+    }
+
+    /**
+     * Sets the president for the turn.
+     *
+     * @param user User to be set as president.
+     * @return <b>true</b> if president was set (they were not currently chancellor or president)
+     */
+    public boolean setPresident(User user) {
+        return setPositionofPower(user, (u) -> president = u);
+    }
+
+    /**
+     * Sets the chancellor for the turn.
+     *
+     * @param user User to be set as chancellor.
+     * @return <b>true</b> if chancellor was set (they were not currently chancellor or president)
+     */
+    public boolean setChancellor(User user) {
+        return setPositionofPower(user, (u) -> chancellor = u);
+    }
+
+    private boolean setPositionofPower(User user, Consumer<User> action) {
+        if(president != null) {
+            if(user.equals(president))
+                return false;
+        }
+
+        if(chancellor != null) {
+            if(chancellor.equals(user))
+                return false;
+        }
+
+        action.accept(user);
         return true;
     }
 
@@ -56,15 +103,7 @@ public class GameController {
         return president;
     }
 
-    public void setPresident(User president) {
-        this.president = president;
-    }
-
     public User getChancellor() {
         return chancellor;
-    }
-
-    public void setChancellor(User chancellor) {
-        this.chancellor = chancellor;
     }
 }
--- src/test/java/io/github/nikmang/shserver/controllers/TestGameController.java
+++ src/test/java/io/github/nikmang/shserver/controllers/TestGameController.java
@@ -1,4 +1,211 @@
 package io.github.nikmang.shserver.controllers;
 
+import io.github.nikmang.shserver.User;
+import io.github.nikmang.shserver.game.GameDeck;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.lang.instrument.UnmodifiableClassException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static junit.framework.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
 public class TestGameController {
+
+    private GameController testGameController;
+
+    @BeforeEach
+    public void setup() {
+        testGameController = new GameController();
+    }
+
+    @Test
+    public void testGetCardsInPlayFresh() {
+        //Given
+        //When
+        List<GameDeck.Card> cards = testGameController.getCardsInPlay();
+
+        //Then
+        assertEquals(3, cards.size());
+    }
+
+    @Test
+    public void testGetCardsInPlayCache() {
+        //Given
+        //When
+        List<GameDeck.Card> cards = testGameController.getCardsInPlay();
+        List<GameDeck.Card> cards2 = testGameController.getCardsInPlay();
+
+        //Then
+        assertEquals(cards, cards2);
+    }
+
+    @Test
+    public void testGetCardsInPlayUnmodifiable() {
+        //Given
+        //When
+        List<GameDeck.Card> cards = testGameController.getCardsInPlay();
+
+        //Then
+        assertThrows(UnsupportedOperationException.class, () -> cards.remove(0));
+    }
+
+    @Test
+    public void testNewDeckGetCardsInPlay() {
+        //Given
+        List<GameDeck.Card> cards = testGameController.getCardsInPlay();
+        testGameController.removeCardFromPlay(2);
+        testGameController.removeCardFromPlay(1);
+        testGameController.removeCardFromPlay(0);
+
+        //When
+        List<GameDeck.Card> testCards = testGameController.getCardsInPlay();
+
+        //Then
+        assertNotSame(cards, testCards);
+    }
+
+    @Test
+    public void testRemoveCardsInvalid() {
+        //Given
+        //When
+        boolean b1 = testGameController.removeCardFromPlay(5);
+        boolean b2 = testGameController.removeCardFromPlay(-1);
+
+        //Then
+        assertFalse(b1);
+        assertFalse(b2);
+    }
+
+    @Test
+    public void testRemoveCardValid() {
+        //Given
+        List<GameDeck.Card> cards = new ArrayList<>(testGameController.getCardsInPlay());
+
+        //When
+        testGameController.removeCardFromPlay(1);
+
+        //Then
+        assertEquals(Arrays.asList(cards.get(0), cards.get(2)), testGameController.getCardsInPlay());
+    }
+
+    @Test
+    public void addPresidentSuccessful() {
+        //Given
+        User user = new User("test-user", null);
+
+        //When
+        boolean b = testGameController.setPresident(user);
+
+        //Then
+        assertEquals(user, testGameController.getPresident());
+        assertTrue(b);
+    }
+
+    @Test
+    public void addPresidentSuccessfullOverride() {
+        //Given
+        User user = new User("test-user", null);
+        User user2 = new User("test-user2", null);
+        testGameController.setPresident(user);
+
+        //When
+        boolean b = testGameController.setPresident(user2);
+
+        //Then
+        assertEquals(user2, testGameController.getPresident());
+        assertTrue(b);
+    }
+
+    @Test
+    public void addPresidentAlreadyPresident() {
+        //Given
+        User user = new User("test-user", null);
+        testGameController.setPresident(user);
+
+        //When
+        boolean b = testGameController.setPresident(user);
+
+        //Then
+        assertEquals(user, testGameController.getPresident());
+        assertFalse(b);
+    }
+
+    @Test
+    public void addPresidentAlreadyChancellor() {
+        //Given
+        User user = new User("test-user", null);
+        testGameController.setChancellor(user);
+
+        //When
+        boolean b = testGameController.setPresident(user);
+
+        //Then
+        assertNull(testGameController.getPresident());
+        assertEquals(user, testGameController.getChancellor());
+        assertFalse(b);
+    }
+
+    @Test
+    public void addChancellorSuccessful() {
+        //Given
+        User user = new User("test-user", null);
+
+        //When
+        boolean b = testGameController.setChancellor(user);
+
+        //Then
+        assertEquals(user, testGameController.getChancellor());
+        assertTrue(b);
+    }
+
+    @Test
+    public void addChancellorSuccessfullOverride() {
+        //Given
+        User user = new User("test-user", null);
+        User user2 = new User("test-user2", null);
+        testGameController.setChancellor(user);
+
+        //When
+        boolean b = testGameController.setChancellor(user2);
+
+        //Then
+        assertEquals(user2, testGameController.getChancellor());
+        assertTrue(b);
+    }
+
+    @Test
+    public void addChancellorAlreadyChancellor() {
+        //Given
+        User user = new User("test-user", null);
+        testGameController.setChancellor(user);
+
+        //When
+        boolean b = testGameController.setChancellor(user);
+
+        //Then
+        assertEquals(user, testGameController.getChancellor());
+        assertFalse(b);
+    }
+
+    @Test
+    public void addChancellorAlreadyPresident() {
+        //Given
+        User user = new User("test-user", null);
+        testGameController.setPresident(user);
+
+        //When
+        boolean b = testGameController.setChancellor(user);
+
+        //Then
+        assertNull(testGameController.getChancellor());
+        assertEquals(user, testGameController.getPresident());
+        assertFalse(b);
+    }
 }
